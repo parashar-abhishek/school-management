1b3234985af81c0d21c00dc301dc05e4e75fcb85
parashar-abhishek <parashar0910@gmail.com>
2025-09-30 12:13:14 +0530

    still testing

--- src/main/java/com/school/management/controller/StudentController.java
+++ src/main/java/com/school/management/controller/StudentController.java
@@ -1,7 +1,6 @@
 package com.school.management.controller;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
